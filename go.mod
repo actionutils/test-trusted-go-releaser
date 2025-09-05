4499e27dc2e7e485968c6b36ebbbab065a00963f
haya14busa <haya14busa@gmail.com>
2025-09-05 10:58:03 +0900

    init

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/actionutils/test-trusted-go-releaser
+
+go 1.25.0
---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("test https://github.com/actionutils/trusted-go-releaser")
+}
